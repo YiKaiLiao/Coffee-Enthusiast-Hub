4094d5845b37430c74546b8b49c23fe0afcbb709
YiKaiLiao <gp6gp6gp6111@gmail.com>
2024-07-18 23:10:16 -0700

    Added CloudFront URL to CORS_ALLOWED_ORIGINS

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://coffee-enthusiast-hub.s3-website.us-east-2.amazonaws.com',
+    'http://d5glfzzfn9tmq.cloudfront.net',
 ]
 
 ROOT_URLCONF = 'server.urls'
